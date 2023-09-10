9f8dd6f01625362ae6fa8e66f6140e85f6934ccc
samuel <samuel.mbula@student.moringaschool.com>
2023-09-10 19:30:37 +0300

    complete lab

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,25 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+    
+    # clear old data
+    session.query(Game).delete()
+    session.commit()
+
+# lib/seed.py
+
+...
+
+print("Seeding games...")
+
+games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60)
+    )
+for i in range(50)]
+
+session.add_all(games)
+session.commit()
